70c49138d321f3d792330f8d2ec4ecc26aaae6f8
Yunji Jong <44912049+yjjong623@users.noreply.github.com>
2019-07-11 17:12:14 -0400

    Create SimPy_Run.sh

---
+++ SimPy_Run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -C avx2
+#SBATCH --job-name=SimPyRun
+#SBATCH --time=00:30
+#SBATCH -n 15
+
+module load Python
+module load parallel
+
+NUM_TASKS=${SLURM_NTASKS}
+
+SRUN="srun --export=all -n1 -N1  --exclusive"
+MODEL_PATH="Lab_HPC/PyUsingSimPy/RunSim2.py"
+MODEL="python $MODEL_PATH z"
+
+PARALLEL_OPTS="-Iz -j$NUM_TASKS"
+
+time seq 15 | parallel $PARALLEL_OPTS $SRUN $MODEL >> file.csv
