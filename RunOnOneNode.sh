4b43ae3d68942a6aef62cc304ec5671efd04fe24
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-07-22 15:14:44 -0400

    moving path to simpy

--- RunOnOneNode.sh
+++ RunOnOneNode.sh
@@ -6,12 +6,12 @@
 module load Python
 module load parallel
 
-#NUM_TASKS=${SLURM_NTASKS}
+NUM_TASKS=${SLURM_NTASKS}
 
 SRUN="srun --export=all -n1 --exclusive"
 MODEL_PATH="SimModel/RunSimOnCluster.py"
 MODEL="python $MODEL_PATH z"
 
-PARALLEL_OPTS="-Iz -j1"
+PARALLEL_OPTS="-Iz -j$NUM_TASKS"
 
 time seq 70 | parallel $PARALLEL_OPTS $SRUN $MODEL >> node.csv
